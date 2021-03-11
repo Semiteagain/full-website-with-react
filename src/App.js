9e5e918c4025d539639dad2cd4c4cc8653479a11
Mukaila Semiu <semiteagain@gmail.com>
2021-03-11 18:57:35 +0100

    add route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import Navbar from './components/Navbar'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Product from './components/Product';
+import Home from './components/Home';
+import Service from './components/Services'
+
 
 
 function App() {
@@ -12,8 +15,11 @@ function App() {
         <Router>
         < Navbar/>
             <Switch>
-                 <Route path='/' exact/>
-                 <Route path='./Product' component={Product} />
+                 <Route path='/' exact component={Home}/>
+                 <Route path='/products' exact component={Product} />
+                 <Route path='/services' component={Service} />
+              
+
             </Switch>
         </Router>
             
